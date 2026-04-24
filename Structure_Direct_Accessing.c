3dc3181caa7a719973bd532dbcf16bbfb1c1ad08
RutujaK-hub <kshirsagarr806@gmail.com>
2026-04-24 21:56:59 +0530

    Diretly access to  structure members

---
+++ Structure_Direct_Accessing.c
@@ -0,0 +1,25 @@
+//Accessing members of structure directly using (.)
+
+#include<stdio.h>
+
+struct Demo
+{
+    int i;      
+    char ch;    
+    float f;    
+};              
+
+int main()
+{
+    struct Demo dobj;
+
+    dobj.i = 11;
+    dobj.ch = 'A';
+    dobj.f = 10.0f;
+
+    printf("%d\n",dobj.i);
+    printf("%c\n",dobj.ch);
+    printf("%f\n",dobj.f);
+
+    return 0 ;
+}
