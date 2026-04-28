b57b6eee3b2dc3844450c48c1d9999cc485e9c6c
RutujaK-hub <kshirsagarr806@gmail.com>
2026-04-29 01:05:13 +0530

    Dynamic Memory Allocation in C using calloc

---
+++ Calloc_Dynamic_Memory.c
@@ -0,0 +1,32 @@
+//// Dynamic Memory Allocation in C - calloc()
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int size = 0;
+
+    float *Marks = NULL;
+
+    printf("Enter the number of students : \n");
+    scanf("%d",&size);
+
+    Marks = (float *) calloc(size,sizeof(float));
+
+    printf("Enter marks of students\n"); 
+    for(int i = 0; i < size; i++)
+    {
+        scanf("%f",&Marks[i]);
+    }
+
+    printf("The marks of students are :\n");
+    for(int i = 0; i<size; i++)
+    {
+        printf("Student %d : %f\n",i+1,Marks[i]);
+    }
+
+    free(Marks);
+
+    return 0;
+}
